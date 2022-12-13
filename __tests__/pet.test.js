faeb91ba2d20986bdf428a5036e112b9ff2e6916
Matthew Giles <matty92g@hotmail.co.uk>
2022-12-13 22:32:26 +0000

    added growUp to Pet Prototype

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,6 +1,19 @@
 const Pet = require('../src/pet');
-describe('constructor', () => {
+describe('Constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+});
+describe('Age', () => {
+    it('has a initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+});
+describe('GrowUp', () => {
+    it('Adds 1 to initial age', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
     });
 });
