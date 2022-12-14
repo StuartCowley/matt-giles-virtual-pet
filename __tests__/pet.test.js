09f0c605c46217989af43954e60b1ef77695a6f1
Matthew Giles <matty92g@hotmail.co.uk>
2022-12-14 20:45:16 +0000

    added hunger and fitness to Pet class

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,10 +10,24 @@ describe('Age', () => {
         expect(pet.age).toEqual(0);
     });
 });
+describe('Hunger', () => {
+    it('has a initial hunger of 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+});
+describe('Fitness', () => {
+    it('has a initial fitness of 10', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    });
+});
 describe('GrowUp', () => {
-    it('Adds 1 to initial age', () => {
+    it('Adds 1 to initial age, adds 5 to initial hunger and reduces fitness by 3', () => {
         const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toEqual(1);
+        expect(pet.hunger).toEqual(5);
+        expect(pet.fitness).toEqual(7);
     });
 });
